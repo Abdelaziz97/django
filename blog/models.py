8ae2c86b291a389858a2a3b987ab75373ef82fa8
Abdelaziz97 <eabdelaziz97@gmail.com>
2019-03-17 18:10:08 +0900

    second

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    lastName = models.CharField(max_length=20)
+    firstName = models.CharField(max_length=20)
+    phoneNumber = models.CharField(max_length=10)
+    firm = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "Client :\n\t" + self.lastName.upper()
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    duration = models.IntegerField()
+    startDate = models.DateTimeField()
+    endDate = models.DateTimeField()
+    client = models.ManyToManyField(Client)
+
+    def __str__(self):
+        return "Project :\n\t" + self.name 
+
+class Task(models.Model):
+    abstract = models.TextField()
+    duration = models.IntegerField()
+    startTime = models.TimeField()
+    endTime = models.TimeField()
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Task :\n\t" + self.abstract
