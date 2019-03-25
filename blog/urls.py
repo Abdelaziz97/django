605a1621dfd6e2c080bdc29ea39d59326ef2d702
Abdelaziz <eabdelaziz97@gmail.com>
2019-03-25 17:31:46 +0900

    third

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from .views import ProjectListView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', ProjectListView.as_view(), name='blog-home'),
     path('client', views.client, name='blog-client'),
     path('user', views.user, name='blog-user'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Client, Project, Task
 
 def home(request):
@@ -7,6 +8,12 @@ def home(request):
         'title': "Bogoss"
     }
     return render(request, 'blog/home.html', context)
+
+class ProjectListView(ListView):
+    model = Project
+    template_name = 'blog/home.html' # default : <app>/<model>_<viewtype>.html
+    context_object_name = 'projects'
+    ordering = ['startDate'] # '-startDate' for an descending order
 
 def client(request):
     return render(request, 'blog/client.html')
